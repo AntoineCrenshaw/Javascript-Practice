9ae97416d8385c1b7bdfe368316f7ae71b9f5e7d
AntoineCrenshaw <Antoine17343@gmail.com>
2023-07-23 15:16:59 -0500

    dgfg

---
+++ discount.js
@@ -0,0 +1,10 @@
+const price = 0;
+const discount = 0;
+const finalPrice = 0;
+
+price = 250;
+discount = 0.40;
+finalPrice = price * (1 - discount);
+
+console.log({ price, discount, finalPrice });
+
